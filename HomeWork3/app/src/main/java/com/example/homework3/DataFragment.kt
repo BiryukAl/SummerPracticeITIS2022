66289200ffd64af2c5794a0791605963ba9ce5bd
BiryukAl <kerzemyak001@gmail.com>
2022-07-03 21:05:01 +0300

    .

--- HomeWork3/app/src/main/java/com/example/homework3/DataFragment.kt
+++ HomeWork3/app/src/main/java/com/example/homework3/DataFragment.kt
@@ -1,9 +1,16 @@
 package com.example.homework3
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class DataFragment : Fragment(R.layout.data_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 
     companion object{
 
--- HomeWork3/app/src/main/java/com/example/homework3/UserHolder.kt
+++ HomeWork3/app/src/main/java/com/example/homework3/UserHolder.kt
@@ -16,6 +16,7 @@ class UserHolder(
 
                 root.setOnClickListener {
                     onItemClick(user)
+
                 }
 
             }
