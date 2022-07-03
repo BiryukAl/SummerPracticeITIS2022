335ed9d3cbc493ccb7a65a91782f86b2e2b38a0d
BiryukAl <kerzemyak001@gmail.com>
2022-07-03 22:11:52 +0300

    Update README.md

--- HomeWork3/app/src/main/java/com/example/homework3/DataFragment.kt
+++ HomeWork3/app/src/main/java/com/example/homework3/DataFragment.kt
@@ -2,13 +2,25 @@ package com.example.homework3
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.example.homework3.databinding.DataFragmentBinding
+import com.example.homework3.databinding.MainFragmentBinding
 
 class DataFragment : Fragment(R.layout.data_fragment) {
+
+    lateinit var binding: DataFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding =DataFragmentBinding.bind(view)
+
+
+
+//        with(binding){
+//            textIdd.text = "id: $iddd"
+//        }
 
     }
 
